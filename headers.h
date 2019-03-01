98d8e00b1f916a52adc1a0d6b2f2d0bb184d18ae
programminghour <44949140+programminghour@users.noreply.github.com>
2019-03-01 09:32:54 +0100

    Create headers.h

---
+++ headers.h
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+using namespace std;
+
+class Pytanie
+{
+    public:
+    
+    string tresc;
+    string poprawna;
+    string a,b,c,d;
+    int nr_pytania;
+    string odp;
+    int pkt;
+    
+};
